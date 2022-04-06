3079a9ca07a38e613acc6fb757eb510cc35f07e3
s2200120 <s2200120@edu.bc.fi>
2022-04-06 15:32:44 +0300

    main added from sonya

---
+++ main.php
@@ -0,0 +1,29 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        
+        
+
+        <form action="ftoc.php" method="post">
+    Fahrenheits to Celcius add fahrenheit: <input type="number" name="ftoc"><br> 
+    Celcius to Fahrenheit add Celcius: <input type="number" name="ctof"><br> 
+    <input type="submit">
+  </form>
+  
+
+    </div>
+    
+</body>
+</html>
