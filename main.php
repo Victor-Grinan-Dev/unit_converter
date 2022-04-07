791f1e4cf557eddda779bbe30c5aafd1533f528d
s2200120 <s2200120@edu.bc.fi>
2022-04-07 14:13:46 +0300

    created className box for css simplicity

--- main.php
+++ main.php
@@ -7,22 +7,29 @@
     <title>Document</title>
 </head>
     <body>
-        <div>   
-            <form action="temperature.php" method="post">
-                Fahrenheits to Celcius add fahrenheit: <input type="number" name="ftoc"><br> 
-                Celcius to Fahrenheit add Celcius: <input type="number" name="ctof"><br> 
-                <input type="submit">
-            </form>   
-            <form action="speed.php" method="post">
-                Kilometers per Hour to Meters per Seconds: <input type="number" name="kmhtoms"><br> 
-                Kilometers per Hour to Knots: <input type="number" name="kmhtok"><br> 
-                <input type="submit">
-            </form> 
-            <form action="mass.php" method="post">
-                Kilograms in grams add kg: <input type="number" name="kgtog"><br>
-                Grams in kg add kg <input type="number" name="gtokg"><br>
-                <input type="submit">
-            </form> 
+        <div class="all elements">   
+
+            <div class="temperature box">
+                <form action="temperature.php" method="post">
+                    Fahrenheits to Celcius add fahrenheit: <input type="number" name="ftoc"><br> 
+                    Celcius to Fahrenheit add Celcius: <input type="number" name="ctof"><br> 
+                    <input type="submit">
+                </form> 
+            </div>
+            <div class="speed box">
+                <form action="speed.php" method="post">
+                    Kilometers per Hour to Meters per Seconds: <input type="number" name="kmhtoms"><br> 
+                    Kilometers per Hour to Knots: <input type="number" name="kmhtok"><br> 
+                    <input type="submit">
+                </form> 
+            </div>
+            <div class="mass box">
+                <form action="mass.php" method="post">
+                    Kilograms in grams add kg: <input type="number" name="kgtog"><br>
+                    Grams in kg add kg <input type="number" name="gtokg"><br>
+                    <input type="submit">
+                </form> 
+            </div>
         </div>
     </body>
 </html>
